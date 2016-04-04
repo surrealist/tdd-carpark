af120b11e9f9dd3e4e92aed02fead5409417493b
Suthep Sangvirotjanaphat <suthep@gfbd.co.th>
2016-04-04 16:29:43 +0700

    add check in and check out pages

--- CarPark/Controllers/HomeController.cs
+++ CarPark/Controllers/HomeController.cs
@@ -3,12 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CarPark.Models;
 
 namespace CarPark.Controllers {
   public class HomeController : Controller {
     public ActionResult Index() {
       return View();
     }
+
+    public ActionResult CheckIn() {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult CheckIn(string PlateNo) {
+      using(var db = new CarParkDb()) {
+        var t = new Ticket();
+        t.PlateNo = PlateNo;
+        t.DateIn = DateTime.Now;
+
+        db.Tickets.Add(t);
+        db.SaveChanges();
+      }
+      return RedirectToAction("CheckIn");
+    }
+
+    public ActionResult CheckOut() {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult CheckOut(int TicketId) {
+      using(var db = new CarParkDb()) {
+        var t = db.Tickets.Find(TicketId);
+        if (t == null) {
+          return HttpNotFound();
+        }
+
+        t.DateOut = DateTime.Now;
+        db.SaveChanges();
+        TempData["LastTicket"] = t;
+      }
+
+      return RedirectToAction("CheckOut");
+    }
+
+
 
     public ActionResult About() {
       ViewBag.Message = "Your application description page.";
@@ -19,14 +59,6 @@ namespace CarPark.Controllers {
     public ActionResult Contact() {
       ViewBag.Message = "Your contact page.";
 
-      return View();
-    }
-
-    public ActionResult CheckIn() {
-      return View();
-    }
-
-    public ActionResult CheckOut() {
       return View();
     }
 
