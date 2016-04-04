f142dcb5ab9ece3d1f5e2a00daed1f3627b1978a
Suthep Sangvirotjanaphat <suthep@gfbd.co.th>
2016-04-04 14:13:37 +0700

    initial commit

---
+++ CarPark.Models/Ticket.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarPark.Models {
+  public class Ticket {
+
+    public string PlateNo { get; set; }
+    public DateTime DateIn { get; set; }
+    public DateTime DateOut { get; set; }
+
+    public decimal ParkingFee {
+      get {
+        TimeSpan ts = DateOut - DateIn;
+
+        ts = ts.Add(TimeSpan.FromMinutes(-15));
+
+        if (ts.TotalMinutes <= 0)
+          return 0m;
+        else if (ts.TotalHours <= 3.0)
+          return 50m;
+        else 
+          return 50m + ((decimal)Math.Ceiling(ts.TotalHours - 3) * 30m);
+        
+      }
+    }
+
+  }
+}
---
+++ CarPark/App_Start/FilterConfig.cs
@@ -0,0 +1,10 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace CarPark {
+  public class FilterConfig {
+    public static void RegisterGlobalFilters(GlobalFilterCollection filters) {
+      filters.Add(new HandleErrorAttribute());
+    }
+  }
+}
